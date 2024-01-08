4041d1daf127fa4ca9d0112acfa476a4d81c624e
Alexander Scarlett <ascarlett92@yahoo.com>
2024-01-08 15:56:36 -0500

    deleted code that was not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 // Added function to call pw generator
+//Add a prompt to ask the user for pw length
 function generatePassword(){
-  var passwordSize=prompt("Enter password length between 8 and 128");
+  var passwordSize=prompt("Enter password length between 8 and 100");
   console.log(passwordSize)
-
-  if(passwordSize>= 8 && passwordSize<=128){
-    
-  }
 }
 // Write password to the #password input
 function writePassword() {
