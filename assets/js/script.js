c1d4d6d4fca199d052dad9de4aeb1167f5d659ed
Alexander Scarlett <ascarlett92@yahoo.com>
2024-01-08 16:02:17 -0500

    Added if statement and an invalid entry prompt

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,16 @@ var generateBtn = document.querySelector("#generate");
 // Added function to call pw generator
 //Add a prompt to ask the user for pw length
 function generatePassword(){
-  var passwordSize=prompt("Enter password length between 8 and 100");
+  var passwordSize=prompt("Enter password length between 8 and 129");
   console.log(passwordSize)
+
+if(passwordSize>= 8 && passwordSize<=128){
+
+}
+else{
+  alert("Invalid Entry, Password length must be between 8 and 128")
+}
+
 }
 // Write password to the #password input
 function writePassword() {
