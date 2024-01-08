527733ad6cb3816d38ca165221183da0b2e365a9
Alexander Scarlett <ascarlett92@yahoo.com>
2024-01-08 16:39:45 -0500

    corrected file path of index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,13 @@ function generatePassword(){
 
 if(passwordSize>= 8 && passwordSize<=128){
     var confirmLowerCase=confirm("Do you want to include lowercase letters in the password?");
+    console.log(confirmLowerCase);
+    var confirmUppercase=confirm("Do you want to include uppercase letters in the password?");
+    var confirmNumeric=confirm("Do you want to include numeric values in the password?");
+    var confirmSpecial=confirm("Do you want to include special characters in the password?");
 }
 else{
-  alert("Invalid Entry, Password length must be between 8 and 128");
+  alert("Invalid Entry, Password length must be between 8 and 128 characters long.");
 }
 
 }
