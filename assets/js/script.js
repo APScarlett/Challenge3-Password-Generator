a30c85c22547d8411bc5ecd70d6a2e9ea3f4f04a
Alexander Scarlett <ascarlett92@yahoo.com>
2024-01-22 17:44:33 -0500

    added if statement for lowercase,number,and uppercase

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,38 +3,64 @@ var generateBtn = document.querySelector("#generate");
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var uppercase = lowercase.toUpperCase();
 consolelog.log(uppercase);
-var number= "0123456789";
-var special= "~!@#$%^&*";
+var number = "0123456789";
+var special = "~!@#$%^&*";
 //var lowercase=uppercase.toLowerCase
 //console.log(uppercase)
 // Added function to call pw generator
 //Add a prompt to ask the user for pw length
-function generatePassword(){
-  var savedPassword= "";
+function generatePassword() {
+  var savedPassword = "";
   var passwordSize = prompt("Enter password length between 8 and 128");
   console.log(passwordSize)
   //passwordsize=7
-  if (passwordSize >= 8 && passwordSize <= 128){
-  var confirmLowerCase=confirm("Do you want to include lowercase letters in the password?");
-  console.log(confirmLowerCase);
-  var confirmUppercase=confirm("Do you want to include uppercase letters in the password?");
-  var confirmNumeric=confirm("Do you want to include numeric values in the password?");
-  var confirmSpecial=confirm("Do you want to include special characters in the password?");
+  if (passwordSize >= 8 && passwordSize <= 128) {
+    var confirmLowerCase = confirm("Do you want to include lowercase letters in the password?");
+    console.log(confirmLowerCase);
+    var confirmUppercase = confirm("Do you want to include uppercase letters in the password?");
+    var confirmNumeric = confirm("Do you want to include numeric values in the password?");
+    var confirmSpecial = confirm("Do you want to include special characters in the password?");
 
 
-if(confirmLowerCase){
-var randomIndex= Math.floor(Math.random() *lowercase.length) // Math.random() multiplied by 26 => extends the range from 0 to 26, this includes decimals
-}
+    for (var i = 0; i < passwordSize; i++) {
 
+    
+    if (confirmLowerCase) {
+        var randomIndex = Math.floor(Math.random() * lowercase.length) // Math.random() multiplied by 26 => extends the range from 0 to 26, this includes decimals
+      
+      console.log(randomIndex)
 
+      savedPassword = savedPassword + lowercase[randomIndex]
+    }
+
+ 
+  if (confirmUppercase) {
+      var randomIndex = Math.floor(Math.random() * uppercase.length)
+   
+    console.log(randomIndex)
+
+    savedPassword = savedPassword + uppercase[randomIndex]
   }
-else{
 
+  if (confirmUppercase) {
+    var randomIndex = Math.floor(Math.random() * number.length)
+ 
+  console.log(randomIndex)
+
+  savedPassword = savedPassword + number[randomIndex]
 }
 
 
+  
+}
 
-return savedPassword
+}
+  else {
+  }
+
+
+
+  return savedPassword
 
 }
 // Write password to the #password input
